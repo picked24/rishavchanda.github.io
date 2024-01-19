e174d6826c6b1ee56f72168be3d61cf1b361f2b8
picked24 <157141714+picked24@users.noreply.github.com>
2024-01-19 21:48:31 +0530

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,7 +4,7 @@ import { InlineWidget } from "react-calendly";
 const App = () => {
   return (
     <div className="App">
-      <InlineWidget url="https://calendly.com/sitecraftdynamics/30min" />
+      <InlineWidget url="https://calendly.com/aiprofits651/30min" />
     </div>
   );
 };
